df3eb6cd586bb35640c8daf9eb5713609134f54b
Aleksey Tatarov <alekseytatarv@gmail.com>
2022-10-12 18:54:32 +0400

    Add app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,127 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+function App(): JSX.Element {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+    <main>
+      <header className="header">
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
+          className="header-logo"
+          href=""
+          target="_top"
           rel="noopener noreferrer"
         >
-          Learn React
+          <img
+            className="header-logo-image"
+            src="images/logo.svg"
+            alt="logotype"
+          />
         </a>
+        <div className="header-line"></div>
       </header>
-    </div>
+
+      <section className="profile">
+        <div className="avatar">
+          <img className="avatar-image" src="images/avatar.jpg" alt="avatar" />
+        </div>
+        <div className="profile-info">
+          <h2 className="profile-info-name">Jacques-Yves Cousteau</h2>
+          <button className="button edit-profile-button" type="button">
+            <img src="images/edit-button.svg" alt="edit" />
+          </button>
+          <p className="profile-info-description">The ocean explorer</p>
+        </div>
+        <button className="button add-button" type="button">
+          +
+        </button>
+      </section>
+
+      <section className="content">
+        <ul className="elements">
+          <li className="element">
+            <img className="element-image" src="images/one.jpg" alt="one" />
+            <div className="element-title">
+              <p className="element-paragraph">Karachaevsk</p>
+              <img className="element-like" alt="like" />
+            </div>
+          </li>
+          <li className="element">
+            <img className="element-image" src="images/two.jpg" alt="two" />
+            <div className="element-title">
+              <p className="element-paragraph">Elbrus</p>
+              <img className="element-like" alt="like" />
+            </div>
+          </li>
+          <li className="element">
+            <img className="element-image" src="images/three.png" alt="three" />
+            <div className="element-title">
+              <p className="element-paragraph">Dombai</p>
+              <img className="element-like" alt="like" />
+            </div>
+          </li>
+          <li className="element">
+            <img className="element-image" src="images/four.png" alt="four" />
+            <div className="element-title">
+              <p className="element-paragraph">Elbrus</p>
+              <img className="element-like" alt="like" />
+            </div>
+          </li>
+          <li className="element">
+            <img className="element-image" src="images/five.jpg" alt="five" />
+            <div className="element-title">
+              <p className="element-paragraph">Dombai</p>
+              <img className="element-like" alt="like" />
+            </div>
+          </li>
+          <li className="element">
+            <img className="element-image" src="images/six.png" alt="six" />
+            <div className="element-title">
+              <p className="element-paragraph">Karachaevo-Cherkesk</p>
+              <img className="element-like" alt="like" />
+            </div>
+          </li>
+        </ul>
+      </section>
+      <div className="edit-wrapper">
+        <div className="edit">
+          <button
+            className="button edit-close-button"
+            type="button"
+            title="Close"
+          ></button>
+          <form className="edit-form" action="#" method="post">
+            <p className="edit-form-header">Edit profile</p>
+
+            <label className="edit-form-label">
+              <input
+                className="edit-form-input edit-form-name"
+                type="text"
+                id="name"
+                name="profile_info_name"
+              />
+            </label>
+
+            <label className="edit-form-label">
+              <input
+                className="edit-form-input edit-form-description"
+                type="text"
+                id="description"
+                name="profile_info_description"
+              />
+            </label>
+
+            <input
+              className="button edit-form-submit"
+              type="submit"
+              id="submit"
+              name="profile_info_submit"
+              value="Save"
+            />
+          </form>
+        </div>
+      </div>
+
+      <footer className="footer">
+        <p className="footer-sign">@2020 Mesto Russia</p>
+      </footer>
+    </main>
   );
 }
 
