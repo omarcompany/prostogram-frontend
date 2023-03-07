05395817ff148014e410358e0159a071d533056a
Aleksey Tatarov <alekseytatarv@gmail.com>
2023-03-07 19:41:40 +0400

    Update design

--- src/components/auth.tsx
+++ src/components/auth.tsx
@@ -63,45 +63,61 @@ export const Auth = ({
   };
 
   return (
-    <div className="auth">
-      <form className="auth-form" onSubmit={callSubmitHandler}>
-        <p className="auth-form-header">{headerTitle}</p>
+    <div className="auth-wrapper">
+      <div className="auth">
+        <form className="auth-form" onSubmit={callSubmitHandler}>
+          <p className="auth-form-header">{headerTitle}</p>
 
-        <label className="auth-form-label">
+          <div className="auth-form-field">
+            <label className="auth-form-field__label">Email</label>
+            <input
+              className="auth-form-field__input"
+              type="text"
+              id="email"
+              name="registration_form_email"
+              placeholder="username@gmail.com"
+              required
+              ref={emailRef}
+              onChange={handleEmailChange}
+            />
+          </div>
+
+          <div className="auth-form-field">
+            <label className="auth-form-field__label"> Password</label>
+            <input
+              className="auth-form-field__input"
+              type="password"
+              id="password"
+              name="registration_form_password"
+              placeholder="At least 5 letters"
+              required
+              ref={passwordRef}
+              onChange={handlePasswordChange}
+            />
+          </div>
           <input
-            className="auth-form-input"
-            type="text"
-            id="email"
-            name="registration_form_email"
-            placeholder="Email"
-            required
-            ref={emailRef}
-            onChange={handleEmailChange}
+            className="button auth-form-submit"
+            type="submit"
+            id="submit"
+            name="registration_form_submit"
+            value={submitTitle}
           />
-        </label>
-
-        <label className="auth-form-label">
-          <input
-            className="auth-form-input"
-            type="password"
-            id="password"
-            name="registration_form_password"
-            placeholder="Password"
-            required
-            ref={passwordRef}
-            onChange={handlePasswordChange}
-          />
-        </label>
-
-        <input
-          className="button auth-form-submit"
-          type="submit"
-          id="submit"
-          name="registration_form_submit"
-          value={submitTitle}
-        />
-        {children}
-      </form>
+          {children}
+        </form>
+      </div>
+      <img
+        className="heart-decoration-image"
+        width={216}
+        height={351}
+        alt={'heart decoration'}
+        src={'/images/heart-decoration.png'}
+      />
+      <div className="auth-background-decoration">
+        <img
+          alt={'auth background decoration'}
+          className="auth-background-decoration__image"
+        ></img>
+      </div>
     </div>
   );
 };
